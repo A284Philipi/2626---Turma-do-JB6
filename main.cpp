be27e0d366df11d88e29bee08e4d2d0cb45805ab
A284Philipi <78768392+A284Philipi@users.noreply.github.com>
2021-05-20 22:08:39 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    string a, b, c;
+    while (cin >> a){
+        cin >> b;
+        cin >> c;
+        if (a[4] == 'a'){
+            if (b[4] == 'a'){
+                if (c[4] == 'a'){
+                    cout << "Putz vei, o Leo ta demorando muito pra jogar..." <<endl;
+                }else{
+                    if (c[4] == 'u'){
+                        cout << "Putz vei, o Leo ta demorando muito pra jogar..." <<endl;
+                    }else{
+                        cout << "Urano perdeu algo muito precioso..." <<endl;
+                    }
+                }
+            }else{
+                if (b[4] == 'u'){
+                    if (c[4] == 'a'){
+                        cout << "Putz vei, o Leo ta demorando muito pra jogar..." <<endl;
+                    }else{
+                        if (c[4] == 'u'){
+                            cout << "Os atributos dos monstros vao ser inteligencia, sabedoria..." <<endl;
+                        }else{
+                            cout << "Putz vei, o Leo ta demorando muito pra jogar..." <<endl;
+                        }
+                    }
+                }else{
+                    if (c[4] == 'a'){
+                        cout << "Iron Maiden's gonna get you, no matter how far!" <<endl;
+                    }else{
+                        if (c[4] == 'u'){
+                            cout << "Putz vei, o Leo ta demorando muito pra jogar..." <<endl;
+                        }else{
+                            cout << "Putz vei, o Leo ta demorando muito pra jogar..." <<endl;
+                        }
+                    }
+                }
+            }
+        }else{
+            if (a[4] == 'u'){
+                if (b[4] == 'a'){
+                    if (c[4] == 'a'){
+                        cout << "Putz vei, o Leo ta demorando muito pra jogar..." <<endl;
+                    }else{
+                        if (c[4] == 'u'){
+                            cout << "Iron Maiden's gonna get you, no matter how far!" <<endl;
+                        }else{
+                            cout << "Putz vei, o Leo ta demorando muito pra jogar..." <<endl;
+                        }
+                    }
+                }else{
+                    if (b[4] == 'u'){
+                        if (c[4] == 'a'){
+                            cout << "Urano perdeu algo muito precioso..." <<endl;
+                        }else{
+                            if (c[4] == 'u'){
+                                cout << "Putz vei, o Leo ta demorando muito pra jogar..." <<endl;
+                            }else{
+                                cout << "Putz vei, o Leo ta demorando muito pra jogar..." <<endl;
+                            }
+                        }
+                    }else{
+                        if (c[4] == 'a'){
+                            cout << "Putz vei, o Leo ta demorando muito pra jogar..." <<endl;
+                        }else{
+                            if (c[4] == 'u'){
+                                cout << "Putz vei, o Leo ta demorando muito pra jogar..." <<endl;
+                            }else{
+                                cout << "Os atributos dos monstros vao ser inteligencia, sabedoria..." <<endl;
+                            }
+                        }
+                    }
+                }
+            }else{
+                if (b[4] == 'a'){
+                    if (c[4] == 'a'){
+                        cout << "Os atributos dos monstros vao ser inteligencia, sabedoria..." <<endl;
+                    }else{
+                        if (c[4] == 'u'){
+                            cout << "Putz vei, o Leo ta demorando muito pra jogar..." <<endl;
+                        }else{
+                            cout << "Putz vei, o Leo ta demorando muito pra jogar..." <<endl;
+                        }
+                    }
+                }else{
+                    if (b[4] == 'u'){
+                        if (c[4] == 'a'){
+                            cout << "Putz vei, o Leo ta demorando muito pra jogar..." <<endl;
+                        }else{
+                            if (c[4] == 'u'){
+                            cout << "Putz vei, o Leo ta demorando muito pra jogar..." <<endl;
+                            }else{
+                                cout << "Iron Maiden's gonna get you, no matter how far!" <<endl;
+                            }
+                        }
+                    }else{
+                        if (c[4] == 'a'){
+                            cout << "Putz vei, o Leo ta demorando muito pra jogar..." <<endl;
+                        }else{
+                            if (c[4] == 'u'){
+                                cout << "Urano perdeu algo muito precioso..." <<endl;
+                            }else{
+                                cout << "Putz vei, o Leo ta demorando muito pra jogar..." <<endl;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
